6467775627b11b5b3e87d1c8796b7599df7d58c6
ali can yücel <yucelalican@hotmail.com>
2022-12-11 17:58:21 +0300

    arge çalışması yapılıyor

--- AngularTask/src/app/app.module.ts
+++ AngularTask/src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
 import { DoktorComponent } from './doktor/doktor.component';
 import { HospitalComponent } from './hospital/hospital.component';
--- AngularTask/src/app/models/doctor.ts
+++ AngularTask/src/app/models/doctor.ts
@@ -1,6 +1,6 @@
 export class DoctorModel {
   id: number= 0;
-  name: string='';
-  lastname: string='';
-  tcno: string='';
+  doctorname: string='';
+  doctorlastname: string='';
+  doctortc: string='';
 }
